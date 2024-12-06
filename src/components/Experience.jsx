02366b090aa019af97b880e5a2ac034de4df4197
Rahul S Kumar <rahul.kumar@qburst.com>
2024-12-06 13:28:26 +0530

    removed rotation of office

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
   return (
     <>
       <ambientLight intensity={2} />
-      <OrbitControls enableZoom={false} />
+      <OrbitControls enableZoom={false} enableRotate={false} />
       <ScrollControls pages={3} damping={0.25}>
         <Overlay />
         <Office />
